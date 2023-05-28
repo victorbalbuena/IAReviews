ccdbea05a9e0b229c93db1fe6815f7b81e67f4da
Victor Balbuena <balbuenabellovictorantonio@gmail.com>
2023-05-27 23:07:34 -0600

    Save review as image function added
    HTML2Canvas added to save reviews as image

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Renderer2, ElementRef, ViewChild} from '@angular/core';
+import html2canvas from "html2canvas";
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  // html element (equals to select an element with Vanilla JS)
+  @ViewChild('canvas') canvas?: ElementRef;
+
+  constructor(private renderer: Renderer2,
+              private el: ElementRef) {}
 
   ngOnInit(): void {
+  }
+
+  captureReview() {
+    // save html element in a variable to use later with html2canvas
+    const element = this.canvas?.nativeElement;
+
+    // html2canvas settings
+    html2canvas((element), {
+      scale: 2
+    }).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+
+      const link = this.renderer.createElement('a');
+      link.href = imgData;
+      // file name
+      link.download = 'review';
+      link.click();
+    })
   }
 
 }
