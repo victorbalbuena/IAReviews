806a7ad9674dcd107641a86104c24bdbd4868a32
Victor Balbuena <balbuenabellovictorantonio@gmail.com>
2023-06-10 17:34:46 -0600

    Design improvements and responsive fixes

--- src/app/components/canva/canva.component.ts
+++ src/app/components/canva/canva.component.ts
@@ -10,6 +10,10 @@ export class CanvaComponent implements OnInit {
   @Input()
   finalReview?: string;
 
+  date = new Date();
+
+  dateFormat = this.date.toUTCString().substring(5, 16);
+
   constructor() { }
 
   ngOnInit(): void {
